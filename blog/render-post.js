ab92f6f357c5bc3a8669a1f7b8439ed9f0c6cf42
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2024-07-03 11:13:35 +0300

    ☕️

--- blog/render-post.js
+++ blog/render-post.js
@@ -20,23 +20,17 @@ function renderPost(options) {
             <link rel="stylesheet" href="/blog/gist-style-overrides.css">
             <script>
                 renderTimestampBelowPostTitle("${options.timestamp}");
-                makeExternalLinksInPostToOpenInNewTabs();
+                fixExternalAndAnchorLinksInPost();
             </script>
         </article>
     `);
 }
 
 function renderTimestampBelowPostTitle(formattedTimestamp) {
-    function html(htmlText) {
-        const template = document.createElement("template");
-        template.innerHTML = htmlText;
-        return Array.from(template.content.children);
-    }
-
     const title = document.querySelector(".gist .markdown-body h1");
     if (!title) { return; }
 
-    const timestamp = html(`
+    const timestamp = _html(`
         <p style="font-size: 85%;">
             <code>${formattedTimestamp}</code>
         </p>`
@@ -44,11 +38,26 @@ function renderTimestampBelowPostTitle(formattedTimestamp) {
     title.after(timestamp);
 }
 
-function makeExternalLinksInPostToOpenInNewTabs() {
+function fixExternalAndAnchorLinksInPost() {
     const links = document.querySelectorAll(".gist a");
     links.forEach((link) => {
-        if (link.href && link.href.startsWith("http")) {
+        const href = link.getAttribute("href");
+        if (!href) { return; }
+
+        if (href.startsWith("http")) {
+            // Open all external links in a new tab/window
             link.target = "_blank";
+        } else if (href.startsWith("#") && link.className == "anchor") {
+            // The actual destination for an anchor link is not available when a Gist is embedded,
+            // so we insert an artificial one right before the corresponding header element
+            const anchor = _html(`<div id="${href.substring(1)}"></div>`)[0];
+            link.parentElement.before(anchor);
         }
     });
+}
+
+function _html(htmlText) {
+    const template = document.createElement("template");
+    template.innerHTML = htmlText;
+    return Array.from(template.content.children);
 }
